7b1f3a1858563ea841f2adaf98428c550f4c6f7e
nahuelriva <nahuelriva@19a95739-31fc-0de1-a8d0-4332442903b0>
2010-11-14 01:03:14 +0000

    * sources for the first release (VC++ express 2008).

---
+++ branches/src/aadp4olly/v0.1/aadp4olly/aadp4olly/aadp4olly.h
@@ -0,0 +1,22 @@
+#include <windows.h>
+#include <tchar.h>
+#include <Strsafe.h>
+#include "plugin.h"
+#include "aadlib.h"
+
+#define VERSIONHI      1
+#define VERSIONLO      10
+
+#define DX             250            
+#define DY             250
+
+#define ID_CB_ISDBGPRESENT 1000
+#define ID_CB_ZWSETINFOTHREAD 1001
+#define ID_CB_ZWQUERYINFOPROCESS 1002
+#define ID_CB_NTGLOBALFLAGS 1003
+#define ID_CB_HEAPFLAGS 1004
+#define ID_CB_ODSTRING 1005
+#define ID_CB_GETTICKCOUNT 1006
+#define ID_BT_SAVE 1007
+#define ID_BT_CANCEL 1008
+
