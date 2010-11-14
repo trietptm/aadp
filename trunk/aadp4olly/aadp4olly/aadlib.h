13ac90a280788039f9967babcb932598150eb3fc
nahuelriva <nahuelriva@19a95739-31fc-0de1-a8d0-4332442903b0>
2010-11-14 01:08:34 +0000

    * sources for the first release (v0.1).

---
+++ trunk/aadp4olly/aadp4olly/aadlib.h
@@ -0,0 +1,16 @@
+// $Id$
+
+#pragma comment(lib, "aadlib.lib")
+
+__declspec(dllexport) bool __stdcall hd_GetTickCount(DWORD pid);
+__declspec(dllexport) bool __stdcall hd_HeapFlagsOnPEB(DWORD pid);
+__declspec(dllexport) bool __stdcall hd_NtGlobalFlags(DWORD pid);
+__declspec(dllexport) bool __stdcall hd_IsDebuggerPresent(DWORD pid);
+__declspec(dllexport) bool __stdcall hd_HookZwQueryInformationProcess(DWORD pid);
+__declspec(dllexport) bool __stdcall hd_HookZwSetInformationThread(DWORD pid);
+__declspec(dllexport) bool __stdcall hd_HookOutputDebugString(DWORD pid);
+
+__declspec(dllexport) HANDLE __stdcall hd_GetProcHandleByPid(DWORD pid);
+__declspec(dllexport) DWORD __stdcall hd_GetProcPidByHandle(HANDLE pHandle);
+__declspec(dllexport) void* __stdcall hd_GetPEBAddress(DWORD pid);
+__declspec(dllexport) bool __stdcall hd_DllInjector(DWORD ProcessId, char* DLLLibPath, bool WaitForResponse);
