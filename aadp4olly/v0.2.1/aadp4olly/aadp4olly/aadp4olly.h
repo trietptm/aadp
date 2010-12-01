a5fdb72ab253c689f162ceaa04d89d4518eff036
nahuelriva <nahuelriva@19a95739-31fc-0de1-a8d0-4332442903b0>
2010-12-01 06:03:17 +0000

    * new minor version to fix bugs in the following functions:  - SuspendThread  - OutputDebugString  - TerminateProcess  - UnhandledExceptionFilter  - Process32Next  - Module32Next
    bugs reported by LCF-AT.

---
+++ aadp4olly/v0.2.1/aadp4olly/aadp4olly/aadp4olly.h
@@ -0,0 +1,15 @@
+#include <windows.h>
+#include <Commctrl.h>
+#include "resource.h"
+#include <tchar.h>
+#include <Strsafe.h>
+#include "plugin.h"
+#include "aadlib.h"
+
+#pragma comment(lib, "comctl32.lib")
+
+#define VERSIONHI		1
+#define VERSIONLO		10
+#define HIGHLIGHTED		1
+#define GRAYED			-1
+#define STANDARD		0
