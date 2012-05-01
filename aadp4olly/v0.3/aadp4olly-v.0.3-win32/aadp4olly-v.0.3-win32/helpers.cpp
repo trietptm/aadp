c44d884a0eb9e86fa8fabae361655135223a7b2b
crackinglandia <crackinglandia@19a95739-31fc-0de1-a8d0-4332442903b0>
2012-05-01 03:53:59 +0000

    * Added place-holders files.

---
+++ aadp4olly/v0.3/aadp4olly-v.0.3-win32/aadp4olly-v.0.3-win32/helpers.cpp
@@ -0,0 +1,21 @@
+// $Id$
+
+/*
+ Anti-Anti-Debugging Plugin for Ollydbg v0.3 - LGPL 3.0
+
+ Copyright (C) 2012 +NCR/CRC! [ReVeRsEr] http://crackinglandia.blogspot.com
+
+ This library is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation, either
+ version 3 of the License, or any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
